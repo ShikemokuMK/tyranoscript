d4fcac8661eb1a35c7d5873c456eeca60fcf50f0
ogihara88sai <ogihara88sai@gmail.com>
2022-07-06 08:00:35 +0900

    feat: ユーティリティにDateフォーマッタを追加 セーブ時刻のフォーマットを`h：m：s`から`hh：mm：ss`に変更

--- tyrano/libs.js
+++ tyrano/libs.js
@@ -113,29 +113,98 @@
         return txtVal;
     };
 
+    const dateFormatter = {
+        _fmt: {
+            hh: function (date) {
+                return ("0" + date.getHours()).slice(-2);
+            },
+            h: function (date) {
+                return date.getHours();
+            },
+            mm: function (date) {
+                return ("0" + date.getMinutes()).slice(-2);
+            },
+            m: function (date) {
+                return date.getMinutes();
+            },
+            ss: function (date) {
+                return ("0" + date.getSeconds()).slice(-2);
+            },
+            dd: function (date) {
+                return ("0" + date.getDate()).slice(-2);
+            },
+            d: function (date) {
+                return date.getDate();
+            },
+            s: function (date) {
+                return date.getSeconds();
+            },
+            yyyy: function (date) {
+                return date.getFullYear() + "";
+            },
+            yy: function (date) {
+                return date.getYear() + "";
+            },
+            t: function (date) {
+                return date.getDate() <= 3 ? ["st", "nd", "rd"][date.getDate() - 1] : "th";
+            },
+            w: function (date) {
+                return ["Sun", "$on", "Tue", "Wed", "Thu", "Fri", "Sat"][date.getDay()];
+            },
+            MMMM: function (date) {
+                return [
+                    "January",
+                    "February",
+                    "$arch",
+                    "April",
+                    "$ay",
+                    "June",
+                    "July",
+                    "August",
+                    "September",
+                    "October",
+                    "November",
+                    "December",
+                ][date.getMonth()];
+            },
+            MMM: function (date) {
+                return ["Jan", "Feb", "$ar", "Apr", "$ay", "Jun", "Jly", "Aug", "Spt", "Oct", "Nov", "Dec"][date.getMonth()];
+            },
+            MM: function (date) {
+                return ("0" + (date.getMonth() + 1)).slice(-2);
+            },
+            M: function (date) {
+                return date.getMonth() + 1;
+            },
+            $: function (date) {
+                return "M";
+            },
+        },
+        _priority: ["hh", "h", "mm", "m", "ss", "dd", "d", "s", "yyyy", "yy", "t", "w", "MMMM", "MMM", "MM", "M", "$"],
+        format: function (date, format) {
+            return this._priority.reduce((res, fmt) => res.replace(fmt, this._fmt[fmt](date)), format);
+        },
+    };
+
+    /**
+     * Dateをフォーマットする
+     * @param {Date} date 日付
+     * @param {string} format フォーマット (例) "yyyy/M/d hh:mm:ss"
+     * @returns {string}
+     */
+    $.formatDate = function (date, format) {
+        return dateFormatter.format(date, format);
+    };
+
     //現在時刻を取得
     //現在の日
-    $.getNowDate = function () {
-        var nowdate = new Date();
-        var year = nowdate.getFullYear();
-        // 年
-        var mon = nowdate.getMonth() + 1;
-        // 月
-        var date = nowdate.getDate();
-        // 日
-
-        return year + "/" + mon + "/" + date;
+    $.getNowDate = function (format = "yyyy/M/d") {
+        return $.formatDate(new Date(), format);
     };
 
     //現在の時刻
-    $.getNowTime = function () {
-        var nowdate = new Date();
-
-        var h = nowdate.getHours();
-        var m = nowdate.getMinutes();
-        var s = nowdate.getSeconds();
-
-        return h + "：" + m + "：" + s;
+    $.getNowTime = function (format = "hh：mm：ss") {
+        return $.formatDate(new Date(), format);
     };
 
     $.convertRem = function (px_val) {
